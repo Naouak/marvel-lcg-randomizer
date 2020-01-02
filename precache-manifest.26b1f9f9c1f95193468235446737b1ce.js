936364c38187e276b8bb00bfb42bd970fe99d9d2
Naouak <tardiel@gmail.com>
2020-01-02 10:58:53 +0000

    Naouak published a site update

---
+++ precache-manifest.26b1f9f9c1f95193468235446737b1ce.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b334d573669e1c7f7f6c",
+    "url": "/marvel-lcg-randomizer/css/app.cb424fd4.css"
+  },
+  {
+    "revision": "ac4d27e6308fae015c02ffdfdec15cfc",
+    "url": "/marvel-lcg-randomizer/favicon.jpg"
+  },
+  {
+    "revision": "8d6185ecdc6ad6fc4441bdf2af642ba7",
+    "url": "/marvel-lcg-randomizer/fonts/FTYSPEEDYCASUAL001NCV.8d6185ec.woff2"
+  },
+  {
+    "revision": "d88edd5a0e04c5a85464ca77161e4775",
+    "url": "/marvel-lcg-randomizer/fonts/FTYSPEEDYCASUAL001NCV.d88edd5a.woff"
+  },
+  {
+    "revision": "7ffcd8bb763d282e017ea39a6c7f87a9",
+    "url": "/marvel-lcg-randomizer/fonts/FTYSPEEDYCASUALNCV.7ffcd8bb.woff2"
+  },
+  {
+    "revision": "a950e08a584e488383a50902c97248f3",
+    "url": "/marvel-lcg-randomizer/fonts/FTYSPEEDYCASUALNCV.a950e08a.woff"
+  },
+  {
+    "revision": "582963aec48ce6b0d0759ed0c1fdd18e",
+    "url": "/marvel-lcg-randomizer/images/aspects/MC01en_55.jpg"
+  },
+  {
+    "revision": "9248a874207eaeb8e3808e1bf58934cd",
+    "url": "/marvel-lcg-randomizer/images/aspects/MC01en_62.jpg"
+  },
+  {
+    "revision": "6d2f0cbee84f7185a198804b0da86752",
+    "url": "/marvel-lcg-randomizer/images/aspects/MC01en_72.jpg"
+  },
+  {
+    "revision": "fe9fc33f15430a20f602a5a7427ee531",
+    "url": "/marvel-lcg-randomizer/images/aspects/MC01en_79.jpg"
+  },
+  {
+    "revision": "d5d6c1516f633a99dc39aa34ab4bded7",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_card_black-panther.png"
+  },
+  {
+    "revision": "ddfba82d162b4974c5da4eabee7c2bdd",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_card_captain-marvel.png"
+  },
+  {
+    "revision": "a0cfc9f22e21831ffe6eac73bc1970f8",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_card_carol-danvers.png"
+  },
+  {
+    "revision": "61f1c24abd1c64fc41b5773a3e5c3723",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_card_jennifer-walters.png"
+  },
+  {
+    "revision": "b03b0cb4634ec88f99c66d2b17743fea",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_card_peter-parker.png"
+  },
+  {
+    "revision": "a3b2c6e54c8727075d36dd0d4f12ca30",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_card_she-hulk.png"
+  },
+  {
+    "revision": "ddd1ccdcf662cb9f9b5bfdfe0e12708e",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_card_spider-man.png"
+  },
+  {
+    "revision": "db5074ecf5cec4134235835f3c119a6f",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_card_tchalla.png"
+  },
+  {
+    "revision": "8cabe4a8ff70ac5bd5ea4ae7961b61f6",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_card_tony-stark.png"
+  },
+  {
+    "revision": "dc8e8cc44ee0e9c2151828c9ddcfa12c",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc01en_iron-man.png"
+  },
+  {
+    "revision": "d1a9010e414503948316eae3b30b6115",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc04en_card_captain-america.png"
+  },
+  {
+    "revision": "2dd26ba0efe333201a00f86ce98ffb94",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc04en_card_steve-rogers.png"
+  },
+  {
+    "revision": "65f55bb6a95293a1fd43bd92036a01d8",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc05en_kamala-khan.png"
+  },
+  {
+    "revision": "c5a73f1ed40e477b94cd47d7e1944a3f",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc05en_ms-marvel.png"
+  },
+  {
+    "revision": "b76e47312a0d6639cc910a6d1fdd0fb8",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc06en_a1_odinson.png"
+  },
+  {
+    "revision": "8eb0ec48c419fdf68f96a4aee5f56b17",
+    "url": "/marvel-lcg-randomizer/images/heroes/mc06en_a1_thor.png"
+  },
+  {
+    "revision": "fdc1e4f0f52fb6e9f924a91d6526c86d",
+    "url": "/marvel-lcg-randomizer/images/missing.png"
+  },
+  {
+    "revision": "afe4782616e946f97af06afcff9ff6d3",
+    "url": "/marvel-lcg-randomizer/images/missing2.png"
+  },
+  {
+    "revision": "e32a822336d67229e345356b8ce632b4",
+    "url": "/marvel-lcg-randomizer/images/modules/0PYOEfh.png"
+  },
+  {
+    "revision": "3e9acd258496fb08907fd1d0f7d9e789",
+    "url": "/marvel-lcg-randomizer/images/modules/IUVMYv5.png"
+  },
+  {
+    "revision": "4120ecfa4aad761f181dbda83c4a049a",
+    "url": "/marvel-lcg-randomizer/images/modules/MC01en_109.jpg"
+  },
+  {
+    "revision": "0dac654ddaa5fa620c56a7ec6d967002",
+    "url": "/marvel-lcg-randomizer/images/modules/MC01en_128.jpg"
+  },
+  {
+    "revision": "4e390c8c7504349c84d528e679dcae97",
+    "url": "/marvel-lcg-randomizer/images/modules/MC01en_151.jpg"
+  },
+  {
+    "revision": "ae21bf4514fb56846a413a97237f8658",
+    "url": "/marvel-lcg-randomizer/images/modules/MC01en_180.jpg"
+  },
+  {
+    "revision": "fa4290bf36b00215967118fc79b68d63",
+    "url": "/marvel-lcg-randomizer/images/modules/MC01en_183.jpg"
+  },
+  {
+    "revision": "558df8307a6a95818573e71c3927bd5d",
+    "url": "/marvel-lcg-randomizer/images/modules/QTUeXkO.png"
+  },
+  {
+    "revision": "6a940f1a1528f952c85448dd3165eb47",
+    "url": "/marvel-lcg-randomizer/images/modules/mc02en_card_goblin-glider.png"
+  },
+  {
+    "revision": "2e2fa8cfb6c591e7543f2801537318a9",
+    "url": "/marvel-lcg-randomizer/images/modules/zP2bl2j.png"
+  },
+  {
+    "revision": "3fb6989b119fe946a30330c11749e187",
+    "url": "/marvel-lcg-randomizer/images/scenarios/3P1fPDH.png"
+  },
+  {
+    "revision": "2bd6575783430b418c1f70e046acdd33",
+    "url": "/marvel-lcg-randomizer/images/scenarios/MC01en_116A.jpg"
+  },
+  {
+    "revision": "c1cb154a0c0a94a6dcdfe742cf31d81f",
+    "url": "/marvel-lcg-randomizer/images/scenarios/MC01en_137A.jpg"
+  },
+  {
+    "revision": "0916ea021a82d706474eb53fc613d0a7",
+    "url": "/marvel-lcg-randomizer/images/scenarios/MC01en_97A.jpg"
+  },
+  {
+    "revision": "e4bea7d399a0400947ba22fe6ae5c6c4",
+    "url": "/marvel-lcg-randomizer/images/scenarios/mc02en_card_hostile-takeover.png"
+  },
+  {
+    "revision": "c507b935248ed5411331a5f9001ffc7f",
+    "url": "/marvel-lcg-randomizer/images/scenarios/mc03en_breakout.png"
+  },
+  {
+    "revision": "d62cfb910790f41348e2242261d68989",
+    "url": "/marvel-lcg-randomizer/images/scenarios/zBayud2.png"
+  },
+  {
+    "revision": "7184bee2a82b964353271a9bfb16ef99",
+    "url": "/marvel-lcg-randomizer/img/logo.7184bee2.jpg"
+  },
+  {
+    "revision": "20009414fc2315af3807e3f4ad1d1155",
+    "url": "/marvel-lcg-randomizer/index.html"
+  },
+  {
+    "revision": "b334d573669e1c7f7f6c",
+    "url": "/marvel-lcg-randomizer/js/app.a69e0fb8.js"
+  },
+  {
+    "revision": "025c4e6a0a8fc3697331",
+    "url": "/marvel-lcg-randomizer/js/chunk-vendors.44e8a185.js"
+  },
+  {
+    "revision": "248722733c0ebf53d056418b2eb5dce0",
+    "url": "/marvel-lcg-randomizer/manifest.json"
+  },
+  {
+    "revision": "915a55b328ce81a6419f753b31d9a6c6",
+    "url": "/marvel-lcg-randomizer/marvel-lcg-randomizer.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/marvel-lcg-randomizer/robots.txt"
+  }
+]);
